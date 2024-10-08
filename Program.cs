9d1b67519f94e5e10880428d90e022ed788d937b
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-08 09:41:21 -0700

    3 taking the string apart

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
-﻿// David Barlow 10/8/2024 Madlibs
+﻿// David Barlow 10/8/2024 Madlibs #1
 Console.Clear();
 
-Console.Write("This is Mad Libs: When you are done providing the requested words");
-Console.Write("\n I will build them into a funny story for you");
+Console.Write("This is Mad Libs: When you are done providing the requested words"); 
+Console.Write("\n I will build them into a funny story for you");   // \n is for a new line it makes it more orgnized
 Console.Write("\n Enter he requested word types");
+Console.WriteLine();
+
+/* This is the orginal story/layout of the story 
+"A vacation is when you take a trip to some (adjective) place with your (adjective) family.
+Usually, you go to some place that is near a/an (noun) or up on a/an (noun).
+A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun)
+I like to spend my time (verb ending in “ing”) or (verb ending in “ing”). 
+When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit
+around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison (plant) 
+all over his (part of the body) My family is going to go to (place) and I will practice (verb ending in “ing”)
+Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number)
+hours every day all year making enough (plural noun) to pay for the vacation."
+*/
+
+string orginalStory = "A vacation is when you take a trip to some (adjective) place with your (adjective) family. Usually, you go to some place that is near a/an (noun) or up on a/an (noun). A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun). I like to spend my time (verb ending in “ing”) or (verb ending in “ing”). When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body) My family is going to go to (place) and I will practice (verb ending in “ing”) Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number) hours every day all year making enough (plural noun) to pay for the vacation.";
+string[] storyWords = orginalStory.Split(' '); //splits up the story into words
+
+for(int i =0; i<storyWords.Length; i++)
+{
+    Console.Write($"{storyWords[i]} ");
+    for(int j=0; j<storyWords.Length; j++)
+    {
+    
+    }
+}
+
+
 Console.WriteLine();
