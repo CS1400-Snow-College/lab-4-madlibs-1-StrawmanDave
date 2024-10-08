e61dd0dccb4a8ae74203f354315e65a4c43e13f8
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-08 09:10:36 -0700

    Greetings complete

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// David Barlow 10/8/2024 Madlibs
+Console.Clear();
+
+Console.Write("This is Mad Libs: When you are done providing the requested words");
+Console.Write("\n I will build them into a funny story for you");
+Console.Write("\n Enter he requested word types");
+Console.WriteLine();
